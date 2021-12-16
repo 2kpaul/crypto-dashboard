557ae1e4d0e9fc5738eb745e9ea215407727a7e7
Paul Maxineanu <paul.maxineanu@gmail.com>
2021-12-16 17:35:32 +0200

    create react components

---
+++ src/conponents/CurrencyConverter.js
@@ -0,0 +1,13 @@
+
+import ExchangeRate from "./ExchangeRate";
+function CurrencyConverter() {
+    return (
+      <div className="currency-converter">
+        Currency converter
+        <ExchangeRate />
+      </div>
+    );
+  }
+  
+  export default CurrencyConverter;
+  
---
+++ src/conponents/ExchangeRate.js
@@ -0,0 +1,10 @@
+function ExchangeRate() {
+    return (
+      <div className="exchange-rate">
+        Excchange Rate
+      </div>
+    );
+  }
+  
+  export default ExchangeRate;
+  
